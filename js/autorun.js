1aaa4dd3773536a28b4774940cb346b7e5f2f810
juanitobebe <juanitobebe@users.noreply.github.com>
2020-05-25 00:08:06 -0700

    Quick hack to load from URL

---
+++ js/autorun.js
@@ -0,0 +1,25 @@
+function readBlob(blob) {
+  return new Promise((resolve, reject) => {
+    var fr = new FileReader();
+    fr.onload = () => {
+      resolve(fr.result);
+    };
+    fr.readAsDataURL(blob);
+  });
+}
+
+function autorun() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const rom = urlParams.get("rom");
+
+  fetch(rom)
+    .then((res) => res.blob()) // Gets the response and returns it as a blob
+    .then(readBlob)
+    .then((readerResult) => {
+      // I probably do't need to do this... but well I need this quick.
+      const rom_b64 = readerResult.replace(/^data:.+;base64,/, "");
+      initPlayer();
+      start(document.getElementById("mainCanvas"), base64_decode(rom_b64));
+    });
+}
